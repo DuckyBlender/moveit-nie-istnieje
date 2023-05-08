a889e1e8cd7408edc820171db92739fbc10cc3e1
Alan Klas <alan@alanklas.com>
2023-05-08 17:00:29 +0200

    added /svg endpoint

--- src/main.rs
+++ src/main.rs
@@ -18,22 +18,55 @@ async fn main() -> Result<(), Error> {
     run(service_fn(move |req| handler(req, lines.clone()))).await
 }
 
-async fn handler(_: Request, lines: Arc<Vec<&str>>) -> Result<impl IntoResponse, Error> {
-    let joke = lines.choose(&mut rand::thread_rng()).unwrap();
+async fn handler(req: Request, lines: Arc<Vec<&str>>) -> Result<impl IntoResponse, Error> {
+    println!("Request: {}", req.uri().path());
+    match req.uri().path() {
+        "/svg" => {
+            let joke = lines.choose(&mut rand::thread_rng()).unwrap();
 
-    // Import the HTML template
-    let html = fs::read_to_string("template.html")
-        .await
-        .expect("Unable to read file");
+            // Import the SVG template
+            let svg = fs::read_to_string("template.svg")
+                .await
+                .expect("Unable to read file");
 
-    // Replace the placeholder with the joke
-    let html = html.replace("{{joke}}", joke);
+            // Replace the placeholder with the joke
+            let svg = svg.replace("{{joke}}", joke);
 
-    // Return the HTML website
-    let response = Response::builder()
-        .status(StatusCode::OK)
-        .header("content-type", "text/html")
-        .body(html)
-        .map_err(Box::new)?;
-    Ok(response)
+            // Return the SVG
+            let response = Response::builder()
+                .status(StatusCode::OK)
+                .header("content-type", "image/svg+xml")
+                .body(svg)
+                .map_err(Box::new)?;
+            Ok(response)
+        }
+
+        "/" => {
+            let joke = lines.choose(&mut rand::thread_rng()).unwrap();
+
+            // Import the HTML template
+            let html = fs::read_to_string("template.html")
+                .await
+                .expect("Unable to read file");
+
+            // Replace the placeholder with the joke
+            let html = html.replace("{{joke}}", joke);
+
+            // Return the HTML website
+            let response = Response::builder()
+                .status(StatusCode::OK)
+                .header("content-type", "text/html")
+                .body(html)
+                .map_err(Box::new)?;
+            Ok(response)
+        }
+        // This should never even happen, as API Gateway should only route to / or /svg
+        _ => {
+            let response = Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(String::from("404 Not Found"))
+                .map_err(Box::new)?;
+            Ok(response)
+        }
+    }
 }
--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 cargo lambda build --release --arm64
 cp target/lambda/moveit-nie-istnieje/bootstrap .
 cp generate/jokes.txt .
-zip lambda.zip bootstrap jokes.txt template.html
+zip lambda.zip bootstrap jokes.txt template.html template.svg
 aws lambda update-function-code --function-name moveit-nie-istnieje --zip-file fileb://lambda.zip
 rm bootstrap lambda.zip jokes.txt

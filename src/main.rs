d8086cf29d464598b1d02f751b8cd05b465776dd
Alan Klas <alan@alanklas.com>
2023-05-08 17:17:39 +0200

    seperated the endpoints to functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use lambda_http::{
     http::{Response, StatusCode},
-    run, service_fn, Error, IntoResponse, Request,
+    run, service_fn, Error, Request,
 };
 use rand::prelude::SliceRandom;
 use std::sync::Arc;
@@ -8,7 +8,6 @@ use tokio::fs;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // Get random line from file
     let jokes = fs::read_to_string("jokes.txt")
         .await
         .expect("Unable to read file");
@@ -18,55 +17,54 @@ async fn main() -> Result<(), Error> {
     run(service_fn(move |req| handler(req, lines.clone()))).await
 }
 
-async fn handler(req: Request, lines: Arc<Vec<&str>>) -> Result<impl IntoResponse, Error> {
+async fn handler(req: Request, lines: Arc<Vec<&str>>) -> Result<Response<String>, Error> {
     println!("Request: {}", req.uri().path());
     match req.uri().path() {
-        "/svg" => {
-            let joke = lines.choose(&mut rand::thread_rng()).unwrap();
-
-            // Import the SVG template
-            let svg = fs::read_to_string("template.svg")
-                .await
-                .expect("Unable to read file");
-
-            // Replace the placeholder with the joke
-            let svg = svg.replace("{{joke}}", joke);
-
-            // Return the SVG
-            let response = Response::builder()
-                .status(StatusCode::OK)
-                .header("content-type", "image/svg+xml")
-                .body(svg)
-                .map_err(Box::new)?;
-            Ok(response)
-        }
-
-        "/" => {
-            let joke = lines.choose(&mut rand::thread_rng()).unwrap();
-
-            // Import the HTML template
-            let html = fs::read_to_string("template.html")
-                .await
-                .expect("Unable to read file");
-
-            // Replace the placeholder with the joke
-            let html = html.replace("{{joke}}", joke);
-
-            // Return the HTML website
-            let response = Response::builder()
-                .status(StatusCode::OK)
-                .header("content-type", "text/html")
-                .body(html)
-                .map_err(Box::new)?;
-            Ok(response)
-        }
-        // This should never even happen, as API Gateway should only route to / or /svg
-        _ => {
-            let response = Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body(String::from("404 Not Found"))
-                .map_err(Box::new)?;
-            Ok(response)
-        }
+        "/svg" => svg_handler(lines).await,
+        "/" => html_handler(lines).await,
+        _ => not_found_handler().await,
     }
+}
+
+async fn svg_handler(lines: Arc<Vec<&str>>) -> Result<Response<String>, Error> {
+    let joke = lines.choose(&mut rand::thread_rng()).unwrap();
+
+    let svg = fs::read_to_string("template.svg")
+        .await
+        .expect("Unable to read file");
+
+    let svg = svg.replace("{{joke}}", joke);
+
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .header("content-type", "image/svg+xml")
+        .body(svg)
+        .map_err(Box::new)?;
+    Ok(response)
+}
+
+async fn html_handler(lines: Arc<Vec<&str>>) -> Result<Response<String>, Error> {
+    let joke = lines.choose(&mut rand::thread_rng()).unwrap();
+
+    let html = fs::read_to_string("template.html")
+        .await
+        .expect("Unable to read file");
+
+    let html = html.replace("{{joke}}", joke);
+
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .header("content-type", "text/html")
+        .body(html)
+        .map_err(Box::new)?;
+    Ok(response)
+}
+
+// this should never be called as we are using API Gateway to route requests
+async fn not_found_handler() -> Result<Response<String>, Error> {
+    let response = Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(String::from("404 Not Found"))
+        .map_err(Box::new)?;
+    Ok(response)
 }
